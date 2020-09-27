0afe2a934b8cde780b1359ab1ae93f5d17d5805d
smallred007 <zjh20021@163.com>
2020-09-27 16:54:04 +0800

    Add Create a new word.

--- src/Hello.java
+++ src/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Say git is a great soft.");
+		System.out.println("Create a new word!");
 	}
 }
